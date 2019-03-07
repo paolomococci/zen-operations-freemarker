3d7c3feac58a8f7f1138e85d23e280578ccd8fbd
paolomococci <37364757+paolomococci@users.noreply.github.com>
2019-03-07 14:49:52 +0100

    Update RefineryProblemTests.java

--- src/test/java/local/example/fm/wrap/RefineryProblemTests.java
+++ src/test/java/local/example/fm/wrap/RefineryProblemTests.java
@@ -93,4 +93,71 @@ public class RefineryProblemTests {
         assertEquals(0.0, result.doubleValue(7), 0.01);
         assertEquals(0.0, result.doubleValue(8), 0.01);
     }
+
+    @Test
+    public void maximizationWithAdvertisingTest() {
+        /*
+        Xij: matrix of decision variables,
+        associates the quantities of barrels of crude oils purchased
+        to the quantities of diesel fuel barrels produced.
+        a1, a2, a3: to advertise diesel fuel one two and three, respectively.
+         */
+        Variable [] objectiveFunction = {
+                new Variable("x11").lower(0).weight(21),
+                new Variable("x12").lower(0).weight(11),
+                new Variable("x13").lower(0).weight(1),
+                new Variable("x21").lower(0).weight(31),
+                new Variable("x22").lower(0).weight(21),
+                new Variable("x23").lower(0).weight(11),
+                new Variable("x31").lower(0).weight(41),
+                new Variable("x32").lower(0).weight(31),
+                new Variable("x33").lower(0).weight(21),
+                new Variable("a1").lower(0).weight(-1),
+                new Variable("a2").lower(0).weight(-1),
+                new Variable("a3").lower(0).weight(-1)
+        };
+        ExpressionsBasedModel model = new ExpressionsBasedModel();
+        model.addVariables(objectiveFunction);
+        Expression applicationConstraintOfDieselFuelTypeOne = model.addExpression().level(3000);
+        Expression applicationConstraintOfDieselFuelTypeTwo = model.addExpression().level(2000);
+        Expression applicationConstraintOfDieselFuelTypeThree = model.addExpression().level(1000);
+        Expression applicationConstraintOfCrudeOilTypeOne = model.addExpression().upper(5000);
+        Expression applicationConstraintOfCrudeOilTypeTwo = model.addExpression().upper(5000);
+        Expression applicationConstraintOfCrudeOilTypeThree = model.addExpression().upper(5000);
+        Expression applicationConstraintProductionOfTheRefinery = model.addExpression().upper(14000);
+        Expression applicationConstraintOfQualityOnTheMinimumNumberOfOctane1 = model.addExpression().lower(0);
+        Expression applicationConstraintOfQualityOnTheMinimumNumberOfOctane2 = model.addExpression().lower(0);
+        Expression applicationConstraintOfQualityOnTheMinimumNumberOfOctane3 = model.addExpression().lower(0);
+        Expression applicationConstraintOfMaximumSulfurContentInCrudeOilType1 = model.addExpression().upper(0);
+        Expression applicationConstraintOfMaximumSulfurContentInCrudeOilType2 = model.addExpression().upper(0);
+        Expression applicationConstraintOfMaximumSulfurContentInCrudeOilType3 = model.addExpression().upper(0);
+        applicationConstraintOfDieselFuelTypeOne.set(0,1).set(1,1).set(2,1).set(9,-10);
+        applicationConstraintOfDieselFuelTypeTwo.set(3,1).set(4,1).set(5,1).set(10,-10);
+        applicationConstraintOfDieselFuelTypeThree.set(6,1).set(7,1).set(8,1).set(11,-10);
+        applicationConstraintOfCrudeOilTypeOne.set(0,1).set(3,1).set(6,1);
+        applicationConstraintOfCrudeOilTypeTwo.set(1,1).set(4,1).set(7,1);
+        applicationConstraintOfCrudeOilTypeThree.set(2,1).set(5,1).set(8,1);
+        applicationConstraintProductionOfTheRefinery.set(0,1).set(1,1).set(2,1).set(3,1).set(4,1).set(5,1).set(6,1).set(7,1).set(8,1);
+        applicationConstraintOfQualityOnTheMinimumNumberOfOctane1.set(0,2).set(1,-4).set(2,-2);
+        applicationConstraintOfQualityOnTheMinimumNumberOfOctane2.set(3,4).set(4,-2);
+        applicationConstraintOfQualityOnTheMinimumNumberOfOctane3.set(6,6).set(8,2);
+        applicationConstraintOfMaximumSulfurContentInCrudeOilType1.set(0,-0.005).set(1,0.01).set(2,0.02);
+        applicationConstraintOfMaximumSulfurContentInCrudeOilType2.set(3,-0.015).set(5,0.01);
+        applicationConstraintOfMaximumSulfurContentInCrudeOilType3.set(6,-0.005).set(7,0.01).set(8,0.02);
+        Result result = model.maximise();
+        assertEquals("", result.getState(), OPTIMAL);
+        assertEquals(277750.0, result.getValue(), 0.0);
+        assertEquals(2000.0, result.doubleValue(0), 0.0);
+        assertEquals(1000.0, result.doubleValue(1), 0.0);
+        assertEquals(0.0, result.doubleValue(2), 0.0);
+        assertEquals(2200.0, result.doubleValue(3), 0.0);
+        assertEquals(4000.0, result.doubleValue(4), 0.01);
+        assertEquals(3300.0, result.doubleValue(5), 0.0);
+        assertEquals(800.0, result.doubleValue(6), 0.0);
+        assertEquals(0.0, result.doubleValue(7), 0.0);
+        assertEquals(200.0, result.doubleValue(8), 0.0);
+        assertEquals(0.0, result.doubleValue(9), 0.0);
+        assertEquals(750.0, result.doubleValue(10), 0.0);
+        assertEquals(0.0, result.doubleValue(11), 0.0);
+    }
 }
