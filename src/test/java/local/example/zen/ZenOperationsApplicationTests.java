be97512fcaefff7b14f5f12085c4ecc74920559e
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-01-11 22:51:57 +0100

    Update ZenOperationsApplicationTests.java

--- src/test/java/local/example/zen/ZenOperationsApplicationTests.java
+++ src/test/java/local/example/zen/ZenOperationsApplicationTests.java
@@ -18,14 +18,11 @@
 
 package local.example.zen;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ZenOperationsApplicationTests {
+class ZenOperationsApplicationTests {
 
 	@Test
 	public void sampleTest() {
