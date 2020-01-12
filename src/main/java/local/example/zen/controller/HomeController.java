5599f394c17ac9bd16c9a7949a8fbd7e9cfca925
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-01-12 10:19:59 +0100

    Update HomeController.java

--- src/main/java/local/example/zen/controller/HomeController.java
+++ src/main/java/local/example/zen/controller/HomeController.java
@@ -24,7 +24,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping(value = {"home", "home.html"})
+    @GetMapping(value = {
+            "home",
+            "home.html"
+    })
     public String home() {
         return "home";
     }
